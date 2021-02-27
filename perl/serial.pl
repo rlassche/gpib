71d5502d96182f95032019aa21e3691a5bf895e3
Your Name <you@example.com>
2021-02-27 21:31:15 +0100

    i.

--- perl/serial.pl
+++ perl/serial.pl
@@ -25,12 +25,78 @@ use Data::Dumper;
 use IO::Select;
 use IO::Handle;
 use Device::SerialPort;
+use File::Slurp qw( read_file);
+
+my $file="1998_init.cmd";
+sub  trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
+my $port;
+my %RACAL_DANA = (
+	"SRS" => { DESCRIPTION=> "SRS command",
+		   ARGS => { "7",  "8" },
+	},
+	"F" =>   { DESCRIPTION=> "FREQuency command Channel B",
+		   ARGS => { "A",  "B" },
+		 },
+	"Q" =>   { DESCRIPTION=> "Q command Channel B",
+		   ARGS => { "A",  "B" },
+		 }
+);
+
+sub send2Device
+{
+	my $cmd= shift ;
+	my @keys = keys %RACAL_DANA;
+	print "TEST: $cmd\n";
+	$port->write( $cmd."\n\r" ) ; 
+	my $p ;
+	return $p ;
+	while( @keys ) {
+		my $k=pop( @keys) ;
+		print "k=$k, cmd=$cmd\n";
+		if( $cmd =~ /$k/ ) {
+		  my $arg = $';
+		  print "FOUND: k=$k: arg=$arg\n";
+		  $port->write( $cmd."\n\r" ) ; 
+		  return $RACAL_DANA{$k};
+		}
+	}
+	print "$cmd not implemented" if( ! defined $p ) ;
+	return $p;
+}
+
+sub initDevice
+{
+	my $file=shift;
+	print "initDevice: $file\n";
+	
+	my @f;
+	open FD, $file ;
+	@f=<FD>;
+	close( FD ) ;
+	for( my $i=0; $i<@f; $i++ ) {
+		my $l=trim( $f[$i] );
+		print "# $l\n";
+		if( $l =~/^#/ || "$l" eq "" ) {
+			print "NEXT ($l)\n";
+			next;
+		}
+		#print "SEND SEND--> $l****\n";
+		my $r=send2Device( "$l" ) ;
+	}
+}
+#die "BYE";
+#die Dumper( \@f ) ;
+#exit;
+my $r;
+#$r=send2Device( "SRS7" ) ;
+#$r=send2Device( "FA" ) ;
+#die Dumper( $r ) ;
 
 my $s = IO::Select->new() ;
 $s->add( \*STDIN ); 
-my $usbDevice='/dev/ttyUSB3' ;
+my $usbDevice='/dev/ttyUSB0' ;
 
-my $port = Device::SerialPort->new($usbDevice);
+$port = Device::SerialPort->new($usbDevice);
 $port->baudrate(9600); # Configure this to match your device
 $port->databits(8);
 $port->parity("none");
@@ -38,14 +104,21 @@ $port->stopbits(1);
 $s->add( $port->{HANDLE} ); 
 print "Add " . Dumper( $port->{HANDLE} ) ;
 
-my $command="SRS7";
-$port->write( $command."\n\r" ) ; 
-$command="FB";
-$port->write( $command."\n\r" ) ; 
-$command="Q7";
-$port->write( $command."\n\r" ) ; 
-$command="RGS";
-$port->write( $command."\n\r" ) ; 
+#my $command="SRS7";
+#$port->write( $command."\n\r" ) ; 
+#send2Device( $command ) ;
+
+#$command="FB";
+#$port->write( $command."\n\r" ) ; 
+#$command="Q7";
+#$port->write( $command."\n\r" ) ; 
+#send2Device( $command ) ;
+
+#$command="RGS";
+#$port->write( $command."\n\r" ) ; 
+
+print "FILE=$file\n";
+initDevice( $file ) ;
 
 my $timeout=20;
 my @ready;
